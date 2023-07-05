cb7af6c37d9acd3755be66f4f3412c3b2528aa8c
Nguyen Hoang Hue <n.huehhh@gmail.com>
2023-07-05 17:18:32 +0700

    CRUD với Node.js và Sequelize - Sử dụng ORM để thêm, hiển thị, sửa, xóa dữ liệu

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
 import db from "../models/index"
-//import CRUDService from "../services/CRUDService";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
     try {
@@ -13,23 +13,59 @@ let getHomePage = async (req, res) => {
     }
 }
 
-// let getCRUD = async (req, res) => {
-//     return res.render('crud.ejs');
-// }
+let getCRUD = async (req, res) => {
+    return res.render('crud.ejs');
+}
 
-// let postCRUD = async (req, res) => {
-//     let message = await CRUDService.createNewUser(req.body);
-//     console.log(message);
-//     return res.send('post crud');
-// }
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud');
+}
 
-// let displayGetCRUD = async (req, res) => {
-//     return res.send('hi');
-// }
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    });
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    } else {
+        return res.send('no');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    });
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send("Xóa tài khoản thành công!")
+    } else {
+        return res.send("Không tìm thấy tài khoản")
+    }
+}
 
 module.exports = {
     getHomePage: getHomePage,
-    // getCRUD: getCRUD,
-    // postCRUD: postCRUD,
-    // displayGetCRUD: displayGetCRUD,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
